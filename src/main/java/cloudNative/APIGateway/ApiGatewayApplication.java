6da8779388216daafd0f41af427c3f62d20395f8
MarcosBB <marcosberaldobarros@hotmail.com>
2025-07-22 09:35:36 -0300

    working gateway

--- src/main/java/cloudNative/APIGateway/ApiGatewayApplication.java
+++ src/main/java/cloudNative/APIGateway/ApiGatewayApplication.java
@@ -2,9 +2,7 @@ package cloudNative.APIGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
 public class ApiGatewayApplication {
 
